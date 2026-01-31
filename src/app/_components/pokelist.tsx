6fbd3f01bae92a020888e06f3a34b519643715cd
pellizas <apfgijon@gmail.com>
2026-01-31 23:52:06 +0100

    centered pills

--- src/app/_components/pokelist.tsx
+++ src/app/_components/pokelist.tsx
@@ -15,7 +15,7 @@ export function ListPokemon() {
           Pokédex
         </h1>
         <FilterBar />
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 justify-items-center">
           {displayList?.map((pokemon) => (
             <PokemonSlot key={pokemon.id} pokemon={pokemon} />
           ))}
